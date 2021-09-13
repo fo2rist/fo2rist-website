246f6d95fe888eadc4e971344f635b4649b1c01e
fo2rist <fo2@inbox.ru>
2021-09-12 19:37:33 -0700

    Clean up post recording social content generation

--- t-kittens_podcast_automation/10_after_recording_auphonic.py
+++ t-kittens_podcast_automation/10_after_recording_auphonic.py
@@ -12,64 +12,24 @@ from podcast_utils import *
 EPISODE_PREFIX = "Выпуск "
 #endregion
 
-def copy_recordings_to(recording_files, target_folder):
+def copy_recordings_to(recording_files, source_folder, target_folder):
+    """Copy files by name from source folder to target folder"""
+    
     for file_name in recording_files:
         shutil.copyfile(
-            join(recording_folder, file_name),
+            join(source_folder, file_name),
             join(target_folder, file_name))
 
-if __name__ == "__main__":    
-    # Sanity check
-    assert_tkittens_podcast_folder()
+def generate_social_network_description(
+    episode_number,
+    recording_date,
+    timings,
+    links,
+    briefs_without_ignored,
+    descriptions,
+):
+    """Create file with templates for social networks"""
 
-    # Locate recordings
-    episode_number = get_last_episode_number()
-    episode_folder = get_last_episode_folder_name()
-
-    try:
-        recording_folder = get_recording_folder_path()
-        recording_files = get_recording_file_names()
-    except FileNotFoundError as err:
-        print(f"Recording folder for last episode doesn't exist: {err}")
-        exit(0)
-
-    # Move single track recording to production folders
-    #  to final Episode folder (primary storage)
-    copy_recordings_to(recording_files, episode_folder)
-    #  to Auphonic Google Drive files for production
-    copy_recordings_to(recording_files, DRIVE_AUPHONIC_FOLDER)
-
-    # Fetch data parts (description, timings, links)
-    recording_date = ""
-    timings = []
-    links = []
-    briefs = []
-    descriptions = []
-    with open(join(episode_folder, DESCRIPTION_FILE_NAME)) as file:
-        for line in file:
-            if date_match := date_regex.match(line):
-                recording_date = date_match[1]
-            if timing_match := timings_regex.match(line):
-                timings.append(timing_match[1])
-            if link_match := links_regex.match(line):
-                links.append(list(link_match[1].split(" ")))
-            if brief_match := theme_briefs_regex.match(line):
-                briefs.append(brief_match[1].strip())
-            if description_match := theme_descriptions_regex.match(line):
-                descriptions.append(description_match[1].strip())
-
-    # Validate the metadata
-    if len(timings) != len(links)\
-            or len(timings) != len(briefs)\
-            or len(timings) != len(descriptions):
-        print(f"Some metadata is missing!\nFiles {recording_files}\nTimings {timings}\nLinks {links}\nBriefs {briefs}\nDescriptions {descriptions}")
-        exit(0)
-
-    # Build common metadata
-    briefs_without_ignored = ", ".join(
-        list(filter(lambda s: not s.startswith('!!'), briefs)))
-
-    # Generate description for social networks posts
     full_title = f"{PODCAST_NAME} #{episode_number}. {briefs_without_ignored}"
     social_description_path = join(episode_folder, POST_SOCIAL_FILE_NAME)
     if (os.path.exists(social_description_path)):
@@ -91,6 +51,62 @@ if __name__ == "__main__":
         social_description_file.write("\nСсылки на новости на странице подкаста: ")
         social_description_file.write("\nМы на Яндекс.Музыке: https://music.yandex.ru/album/12017408\n")
 
+def is_data_complete(timings, links, briefs, descriptions):
+    return len(timings) != len(links) or\
+        len(timings) != len(briefs) or\
+        len(timings) != len(descriptions)
+
+if __name__ == "__main__":    
+    # Sanity check
+    assert_tkittens_podcast_folder()
+
+    # Locate recordings
+    episode_number = get_last_episode_number()
+    episode_folder = get_last_episode_folder_name()
+
+    try:
+        recording_folder = get_recording_folder_path()
+        recording_files = get_recording_file_names()
+    except FileNotFoundError as err:
+        print(f"Recording folder for last episode doesn't exist: {err}")
+        exit(0)
+
+    # Move single track recording to production folders
+    #  to final Episode folder (primary storage)
+    copy_recordings_to(recording_files, recording_folder, episode_folder)
+    #  to Auphonic Google Drive files for production
+    copy_recordings_to(recording_files, recording_folder, DRIVE_AUPHONIC_FOLDER)
+
+    # Fetch episode data parts (description, timings, links, etc.)
+    recording_date = ""
+    timings = []
+    links = []
+    briefs = []
+    descriptions = []
+    with open(join(episode_folder, DESCRIPTION_FILE_NAME)) as file:
+        for line in file:
+            if date_match := date_regex.match(line):
+                recording_date = date_match[1]
+            if timing_match := timings_regex.match(line):
+                timings.append(timing_match[1])
+            if link_match := links_regex.match(line):
+                links.append(list(link_match[1].split(" ")))
+            if brief_match := theme_briefs_regex.match(line):
+                briefs.append(brief_match[1].strip())
+            if description_match := theme_descriptions_regex.match(line):
+                descriptions.append(description_match[1].strip())
+    briefs_without_ignored = ", ".join(
+        list(filter(lambda s: not s.startswith('!!'), briefs)))
+
+    # Validate the metadata
+    if is_data_complete(timings, links, briefs, descriptions):
+        print(f"Some metadata is missing! Check:\nFiles {recording_files}\nTimings {timings}\nLinks {links}\nBriefs {briefs}\nDescriptions {descriptions}")
+        exit(0)
+
+    # Create content for social networks
+    generate_social_network_description(episode_number, recording_date, timings, links, briefs_without_ignored, descriptions,)
+
+    # Initiate audio rendering at Auphonic
     create_auphonic_production(
         episode_number = episode_number,
         # Title - prefixed episode number
